87ba95a8b036f47886b8e6dfaf506d8ba9f412de
mohammed muneesh E K <mohammedmuneesh1@gmail.com>
2023-12-09 12:20:43 +0530

    Card-Add To Cart - Added

--- src/Components/Body/Popular.jsx
+++ src/Components/Body/Popular.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Card } from '../Card'
+import { Card1 } from '../Card1'
 export const Popular = () => {
   return (
     <div className='p-3'>
       <h1 className=' ml-4 sm:text-2xl font-serif text-bold'>Popular Books</h1>
-      <div className='mt-10 mb-30 mx-auto max-w-[85%]  text-xl flex  flex-row flex-nowrap justify-around bg-pink-300 font-poppin '>
+      <div className='mt-10  sm:mb-20 mb-10    mx-auto max-w-[85%]  text-xl flex  flex-row flex-nowrap justify-around bg-pink-300 font-poppin '>
         <span>All</span>
         <span>Fiction</span>
         <span>Business</span>
@@ -14,11 +14,11 @@ export const Popular = () => {
         <span>Kids Category</span>
       </div>
       <div className='flex flex-wrap justify-around  max-w-[100%] mx-auto mb-5 '>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card1 />
+        <Card1 />
+        <Card1 />
+        <Card1 />
+        <Card1 />
       </div>
     </div>
   )
--- src/Components/Card1.jsx
+++ src/Components/Card1.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import atomic from '../Components/Assest/atomic-habit.jpg'
+
 
 export const Card1 = () => {
   return (
-    <div>
-        
+    <div className='max-w-[250px] min-h-[400px] border-2  px-3 pt-3 mb-3 ' >
+    <img src={atomic} className='overflow-hidden w-screen h-[220px]' />
+    <div className='text-center'>
+    {/* <h3 className='leading-5'>This is some text with line height 1.5</h3> sm:text-lg font-semibold */}
+    {/* <h3 className=' sm-text-lg leading-5 mt-1'>Atomic Habit Tiny Changes by magic points</h3> */}
+
+        <h3 className=' leading-3 sm:text-lg  font-semibold  mt-1'>Atomic Habit Tiny Changes by magic points</h3>
+        <span className=' text-sm text-gray-400 font-xs'> By James Clear</span>
+        <br/>
+        <span className='font-semibold'>₹40.99</span>
+        <br/>
+        <button className='mt-1 rounded-sm bg-red-500 p-2 text-white text-sm '>Add to Cart </button>
+    </div>
     </div>
   )
 }
